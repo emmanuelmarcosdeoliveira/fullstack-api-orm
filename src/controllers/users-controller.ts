a8c9da1d8ca7f82fbf58c710fd9fecb83e464b7d
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-06-16 23:40:56 -0300

    feat: created the methods to create and list users with prisma

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,11 +1,14 @@
 import { Request, Response } from "express";
-
+import { prisma } from "@/prisma";
 class UsersController {
   async index(request: Request, response: Response) {
-    return response.json();
+    const users = await prisma.user.findMany();
+    return response.json(users);
   }
 
   async create(request: Request, response: Response) {
+    const { name, email } = request.body;
+    await prisma.user.create({ data: { name, email } });
     return response.status(201).json();
   }
 
